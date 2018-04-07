e1858df383910b93f10b55f56a52e6bbd224405d
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-07 23:13:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.size == 1
+  if array.size === 1
     return array.join
-  else if array.size == 2
+  else if array.size === 2
     array.join(" and ")
   else
     array[array.size - 1] = "and " + array[array.size - 1]
